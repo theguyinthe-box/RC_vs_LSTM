515817c4ea9af90138f72f59e6c0c57809e31ab2
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-09-27 14:36:46 -0700

    .x

--- ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
+++ ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
@@ -3,8 +3,8 @@ from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray, String
 import jax
 import jax.numpy as np
-from ..reservoirpy.jax.nodes import Ridge, Reservoir
-from ..reservoirpy.datasets import set_seed
+from .reservoirpy.jax.nodes import Ridge, Reservoir
+from .reservoirpy.datasets import set_seed
 import joblib
 import time
 import os
