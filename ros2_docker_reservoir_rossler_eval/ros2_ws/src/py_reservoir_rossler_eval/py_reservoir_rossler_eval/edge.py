700691c27d0a490f9a119af74f97e1cd2fd23433
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-09-28 23:38:25 -0700

    something maybew

--- ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
+++ ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
@@ -177,11 +177,13 @@ class EdgeReservoirNode(Node):
         predictions = jnp.array([])
         last_input = X[-1].reshape(1, -1)
         timings = jnp.array([])
+        
         model.reset()
+        model.run(X[-100:], workers=-1)
 
         for _ in range(test.shape[0]):
             start = time.perf_counter()
-            pred = model(last_input, workers=-1)
+            pred = model(last_input)
             end = time.perf_counter()
             timings = jnp.append(timings, end - start)
             predictions = jnp.append(predictions, pred.ravel())
