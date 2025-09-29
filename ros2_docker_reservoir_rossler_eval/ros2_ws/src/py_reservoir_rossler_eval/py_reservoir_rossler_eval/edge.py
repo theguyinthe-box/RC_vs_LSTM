e3d125049e95092e018ef005e874a847d381849a
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-09-28 22:07:20 -0700

    whitespace delete

--- ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
+++ ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
@@ -180,7 +180,7 @@ class EdgeReservoirNode(Node):
 
         for _ in range(test.shape[0]):
             start = time.perf_counter()
-            pred = model.run (last_input, workers=-1)
+            pred = model.run(last_input, workers=-1)
             model.reset()
             end = time.perf_counter()
             timings = jnp.append(timings, end - start)
