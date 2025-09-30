91f5d4e38904c5ba374cdb9b04b0b5ff4a3662e2
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-09-29 20:26:25 -0700

    bloop bloop bloop

--- ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
+++ ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
@@ -157,7 +157,7 @@ class EdgeReservoirNode(Node):
             model = reservoir >> readout
 
             start_time = time.perf_counter()
-            model = model.fit(X, Y, warmup=100)#, reset=True) deprecated reset in respy0.4.1 current resets by default
+            model = model.fit(X, Y, warmup=50)#, reset=True) deprecated reset in respy0.4.1 current resets by default
             training_duration = time.perf_counter() - start_time
 
             #joblib.dump(model, self.model_path)
