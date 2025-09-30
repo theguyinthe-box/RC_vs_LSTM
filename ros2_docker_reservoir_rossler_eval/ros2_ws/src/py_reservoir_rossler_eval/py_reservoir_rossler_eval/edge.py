5ce9a17908eef4c893319337840be8f13f839276
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-09-29 19:05:54 -0700

    iters maybe?

--- ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
+++ ros2_docker_reservoir_rossler_eval/ros2_ws/src/py_reservoir_rossler_eval/py_reservoir_rossler_eval/edge.py
@@ -180,7 +180,7 @@ class EdgeReservoirNode(Node):
 
         for _ in range(test.shape[0]):
             start = time.perf_counter()
-            pred = model.call(last_input)
+            pred = model.run(iters=1,workers=-1)
             end = time.perf_counter()
             timings = jnp.append(timings, end - start)
             predictions = jnp.append(predictions, pred.ravel())
